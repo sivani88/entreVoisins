03f1cfd9bee067856014ccd4bc57c5a14e979715
Sivani88 <sivani88@yahoo.fr>
2021-09-24 14:03:30 +0200

    commit du 24/09/2021 test fini est reusi

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -26,6 +26,7 @@ import java.util.List;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.core.IsNull.notNullValue;
 
@@ -86,31 +87,48 @@ public class NeighboursListTest {
 
 
     @Test
+    public void starChangeForm() {
+
+        onView(withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(6, ViewActions.click()));
+        onView(withId(R.id.floatingActionButton3)).check(matches(new DrawableMatcher(R.drawable.ic_star_yellow)));
+        onView(withId(R.id.floatingActionButton3)).perform(ViewActions.click());
+        onView(withId(R.id.floatingActionButton3)).check(matches(new DrawableMatcher(R.drawable.ic_star_pleine_yellow)));
+
+    }
+
+    @Test
+    public void infoActivity() {
+        onView(withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(3, ViewActions.click()));
+        onView(withId(R.id.prenomVoisinCard)).check(matches(withText("Elodie")));
+
+    }
+
+    @Test
+    public void deleteFavoriteAndCheck() {
+        service = DI.getNeighbourApiService();
+        List<Neighbour> favoriteNeighbours = service.getFavoriteNeighbours();
+        int nbrFavorite = favoriteNeighbours.size();
+
+
+        onView(withId(R.id.list_neighbours)).perform(ViewActions.swipeLeft());
+
+        onView(withId(R.id.favoris_list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
+        onView(withId(R.id.favoris_list_neighbours)).check(withItemCount(nbrFavorite - 1));
+    }
+
+    @Test
     public void addFavoritesAndCheck() {
-       service = DI.getNeighbourApiService();
+        service = DI.getNeighbourApiService();
         List<Neighbour> favoriteNeighbours = service.getFavoriteNeighbours();
         int nbrFavorite = favoriteNeighbours.size();
 
         onView(withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(5, ViewActions.click()));
         onView(withId(R.id.floatingActionButton3)).perform(ViewActions.click());
         onView(withId(R.id.imageButtonArriere)).perform(ViewActions.click());
-        onView(withId(R.id.list_neighbours)).perform(RecyclerViewActions.scrollToPosition(10));
-        onView(withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(10, ViewActions.click()));
-        onView(withId(R.id.floatingActionButton3)).perform(ViewActions.click());
-        onView(withId(R.id.imageButtonArriere)).perform(ViewActions.click());
+
 
         onView(withId(R.id.list_neighbours)).perform(ViewActions.swipeLeft());
-        onView(withId(R.id.favoris_list_neighbours)).check(withItemCount(nbrFavorite + 2));
-
-    }
-
-    @Test
-    public void starChangeForm() {
-
-        onView(withId(R.id.list_neighbours)).perform(RecyclerViewActions.actionOnItemAtPosition(5, ViewActions.click()));
-        onView(withId(R.id.floatingActionButton3)).check(matches(new DrawableMatcher(R.drawable.ic_star_yellow)));
-        onView(withId(R.id.floatingActionButton3)).perform(ViewActions.click());
-        onView(withId(R.id.floatingActionButton3)).check(matches(new DrawableMatcher(R.drawable.ic_star_pleine_yellow)));
+        onView(withId(R.id.favoris_list_neighbours)).check(withItemCount(nbrFavorite + 1));
 
     }
 
